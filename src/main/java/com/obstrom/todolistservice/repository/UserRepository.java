28733d1b142948bf1fac2192c40e262ccc47aeb3
Oscar Bergström <oscar@obstrom.com>
2022-04-30 17:42:35 +0200

    User repository using CrudRepository

---
+++ src/main/java/com/obstrom/todolistservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.obstrom.todolistservice.repository;
+
+import com.obstrom.todolistservice.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
+}
