e558b224f0c34b0d98e69c897272a66c58a6d90e
Oscar Bergström <oscar@obstrom.com>
2022-04-30 17:33:53 +0200

    Init

---
+++ src/test/java/com/obstrom/todolistservice/TodolistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obstrom.todolistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
