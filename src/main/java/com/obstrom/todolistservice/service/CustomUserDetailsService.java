3e38cb6e82644537ef80f9e18712126dce48765e
Oscar Bergström <oscar@obstrom.com>
2022-05-04 22:06:22 +0200

    Refactor custom UserDetailsService

--- src/main/java/com/obstrom/todolistservice/service/CustomUserDetailsService.java
+++ src/main/java/com/obstrom/todolistservice/service/CustomUserDetailsService.java
@@ -4,18 +4,19 @@ import com.obstrom.todolistservice.model.User;
 import com.obstrom.todolistservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Autowired
-    public UserDetailsService(UserRepository userRepository) {
+    public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
